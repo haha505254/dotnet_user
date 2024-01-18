e6e70159ae2ebd50f55da5f4885d8c3b0c0cd5d8
haha505254 <haha505254@gmail.com>
2024-01-18 14:46:45 +0800

    temp success excel download

--- Controllers/BiopsyController.cs
+++ Controllers/BiopsyController.cs
@@ -12,8 +12,10 @@ using System;
 using System.Globalization;
 using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace dotnet_user.Controllers
+
 {
     [Route("切片報告管理")]
     public class BiopsyController : Controller
@@ -30,7 +32,6 @@ namespace dotnet_user.Controllers
             _logger = logger;
 
         }
-
         public IActionResult Index()
         {
             var strDate = _dateService.GetStartDate(30);
@@ -46,9 +47,61 @@ namespace dotnet_user.Controllers
         public async Task<IActionResult> Biopsy(string str_date = "", string end_date = "")
         {
             var result = await _biopsyService.GetBiopsyData(str_date, end_date);
-            return Json(result);
+            var json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            return Content(json, "application/json");
         }
 
+        [HttpGet("downloadDetail")]
+        public async Task<IActionResult> DownloadDetail(string str_date = "", string end_date = "")
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            // 從服務獲取動態數據
+            var dynamicData = await _biopsyService.GetBiopsyData(str_date, end_date);
+
+            // 將動態數據轉換為強類型集合
+            var records = new List<BiopsyData>();
+            foreach (var item in dynamicData)
+            {
+                var record = new BiopsyData
+                {
+                    來源 = item.來源,
+                    Counter = item.counter,
+                    處置檔 = item.處置檔,
+                    開單日期 = item.開單日期,
+                    病歷號碼 = item.病歷號碼,
+                    姓名 = item.姓名,
+                    科別 = item.科別,
+                    醫師 = item.醫師
+                };
+                records.Add(record);
+            }
+
+            // 創建 Excel 文件
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Biopsy Data");
+                worksheet.Cells["A1"].LoadFromCollection(records, true);
+
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "BiopsyData.xlsx");
+            }
+        }
     }
+    public class BiopsyData
+    {
+        public string? 來源 { get; set; }
+        public string? Counter { get; set; }
+        public string? 處置檔 { get; set; }
+        public string? 開單日期 { get; set; }
+        public string? 病歷號碼 { get; set; }
+        public string? 姓名 { get; set; }
+        public string? 科別 { get; set; }
+        public string? 醫師 { get; set; }
+    }
+
 
 }
